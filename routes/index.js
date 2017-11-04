258fd3ce0d9f0d44e4bf976c2f7172524664ab0a
yoruno07 <ryo.yoru.io@gmail.com>
2017-11-04 23:30:48 +0900

    画面表示テスト

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,28 @@ var modules = require('../module');
 
 var T = modules.twitter;
 
-console.log(T);
-console.log(T.getAuth());
+var keyword = "メンテナンス";
+var from_account = "fgoproject";
+
+var results = [];
+
+T.get('search/tweets', { q: keyword + ' from:' + from_account}, function(err, data, response) {
+     if  (err) {
+        return console.log("ERROR: " + err);
+     }
+      var statuses = data.statuses;
+      var max_count = statuses.length;
+      console.log(max_count);
+      for (i=0; i < max_count; i++) {
+        results.push(statuses[i].text);
+      }
+});
+
 
 router.get('/', function(req, res, next) {
     res.render('index',
         {title : 'メンテチェッカー' ,
-        content: 'ここにコンテンツを表示'}
+        results: results}
     );
 });
 
