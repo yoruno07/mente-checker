ff9e4060e9015c2b094b6d8abfafbc673dcc6da9
yoruno07 <ryo.yoru.io@gmail.com>
2017-11-04 21:02:08 +0900

    module追加

--- app.js
+++ app.js
@@ -2,49 +2,36 @@ var express = require('express');
 var path = require('path');
 var routes = require('./routes/index');
 var bodyParser = require('body-parser');
-var twit = require('twit');
+var Twit = require('twit');
 
 var app = express();
+
+var options = {
+  token: process.env.ACCESS_TOKEN,
+  token_secret: process.env.ACCESS_TOKEN_SECRET,
+  key: process.env.CONSUMER_KEY,
+  secret: process.env.CONSUMER_SECRET
+};
+app.set('options', options);
+
+var T = new Twit({
+  consumer_key: options.key,
+  consumer_secret: options.secret,
+  access_token: options.token,
+  access_token_secret: options.token_secret
+});
+
+console.log(T.getAuth());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('listening on port ' + process.env.PORT);
+app.listen(app.get('port'), function () {
+  console.log('listening on port ' +  app.get('port'));
 });
 
-
-// コマンドライン引数をキーワードとして検索する
-// var keyword = process.argv[2];
-// var from_account = process.argv[3];
-
-// var keyword = "メンテナンス";
-// var from_account = "fgoproject";
-
-// console.log("「" + keyword + "」" + "についてtwitter検索します");
-
-// twitter.get('search/tweets', { q: keyword, count: 20 }, function(err, data, response) {
-// twitter.get('search/tweets', { q: keyword + ' from:' + from_account, count: 20 }, function(err, data, response) {
-//  if  (err) {
-//     return console.log("ERROR: " + err);
-//  }
-//   var statuses = data.statuses;
-//   var max_count = statuses.length;
-//   console.log(max_count);
-//   for (i=0; i < max_count; i++) {
-//     console.log(statuses[i].text);
-//     console.log("---------------------------------------------------------");
-//   }
-// });
-
-// var stream = twitter.stream('statuses/filter', {track: keyword});
-
-// stream.on('tweet', function(tweet){
-//   var text = tweet.text;
-//   var user_name = tweet.user.name;
-//   console.log("ユーザー名: " + user_name + "\nツイート: " + text);
-//   console.log("=========================================================");
-// });
+// module.exports = Twitter;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+// var T = require('../app');
 var router = express.Router();
+
+
+
 
 router.get('/', function(req, res, next) {
     res.render('index',
