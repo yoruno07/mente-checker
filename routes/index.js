7fd58d3fe949e9277731de38905ae8a9f88549ce
yoruno07 <ryo.yoru.io@gmail.com>
2017-11-06 15:06:51 +0900

    socket.ioでtweetをリアルタイム表示

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var modules = require('../module');
+// var modules = require('../module');
 
-var T = modules.twitter;
+// var T = modules.twitter;
 
-var keyword = "メンテナンス";
-var from_account = "fgoproject";
+// var keyword = "メンテナンス";
+// var from_account = "fgoproject";
 
-var results = [];
+// var results = [];
 
-var info = T.getAuth();
+// var info = T.getAuth();
 
-T.get('search/tweets', { q: keyword + ' from:' + from_account}, function(err, data, response) {
-     if  (err) {
-        return console.log("ERROR: " + err);
-     }
-      var statuses = data.statuses;
-      var max_count = statuses.length;
-      console.log(max_count);
-      for (i=0; i < max_count; i++) {
-        results.push(statuses[i].text);
-      }
-});
+// T.get('search/tweets', { q: keyword + ' from:' + from_account}, function(err, data, response) {
+//      if  (err) {
+//         return console.log("ERROR: " + err);
+//      }
+//       var statuses = data.statuses;
+//       var max_count = statuses.length;
+//       console.log(max_count);
+//       for (i=0; i < max_count; i++) {
+//         results.push(statuses[i].text);
+//       }
+// });
 
 
 router.get('/', function(req, res, next) {
     res.render('index',
-        {title : 'メンテチェッカー' ,
-        info : info,
-        results: results}
+        {title : 'メンテチェッカー'}
     );
 });
 
