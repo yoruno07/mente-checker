79481a5483bec470a3b479bc27b3885211e23c79
yoruno07 <ryo.yoru.io@gmail.com>
2017-11-08 17:26:12 +0900

    twitterへのリンク追加・説明文追加

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index',
     {title : config.sitename,
     description : config.description,
+    twitter_url : config.twitter_url,
     games: config.games}
   );
 });
