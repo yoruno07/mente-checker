49f14688edd1f998f79a7970622ca01acbfb4d6a
yoruno07 <ryo.yoru.io@gmail.com>
2017-11-08 18:47:04 +0900

    reconnect修正

--- public/javascripts/info.js
+++ public/javascripts/info.js
@@ -1,15 +1,18 @@
 // JSONデータを元に情報を取得するゲームの分だけsocketを待ち受け状態にする
 $(function() {
-  var socket = io.connect();
+  var socket = io.connect({'reconnect': true});
   // 接続時に既存の要素は一旦削除
   socket.on('connect', function(data) {
-    $('ul').empty();
-  });
-  $.getJSON("/json/config.json", function(data) {
-    $.each(data.games, function(key, val) {
-      socket.on(val.eventname, function(info) {
-        $('ul#' + val.id).prepend('<li class="list-group-item"> '+ info + '</li>');
+    $('ul').children().remove();
+    console.log('delete');
+      $.getJSON("/json/config.json", function(data) {
+        $.each(data.games, function(key, val) {
+          socket.on(val.eventname, function(info) {
+            console.log('msg');
+            $('ul#' + val.id).prepend('<li class="list-group-item"> '+ info + '</li>');
+          });
+        });
       });
-    });
   });
+
 });
