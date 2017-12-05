0ebb49c58edcdc5cf99804e5b3d4c791ca0cf950
yoruno07 <ryo.yoru.io@gmail.com>
2017-12-05 18:08:31 +0900

    スマホ時は並び替え無効にする

--- public/javascripts/sortable.js
+++ public/javascripts/sortable.js
@@ -1,9 +1,11 @@
-// 各メンテ情報をドラッグで場所を並び替えられるようにする
-$(function() {
-  $(".card-columns").sortable({
-    item: ".card",
-    cursor: "move",
-    opacity: "0.7",
-    connectWith:".card-columns"
-  });
-});
+// 各メンテ情報をドラッグで場所を並び替えられるようにする（スマホ以外）
+if (!navigator.userAgent.match(/(iPhone|iPod|Android)/)) {
+    $(function() {
+      $(".card-columns").sortable({
+        item: ".card",
+        cursor: "move",
+        opacity: "0.7",
+        connectWith:".card-columns"
+      });
+    });
+}
